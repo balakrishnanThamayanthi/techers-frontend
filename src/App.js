8d0c5feb2caeb236cf0b3d4c0c8368846e39664b
dharani <dharaniba@gmail.com>
2023-07-06 21:21:39 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home';
+import { Department } from './Department';
+
+import { BrowserRouter, Route, NavLink, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <div className="App container">
+      <h3 className='d-flex justify-content-conter m-3'>Hospital Management System</h3>
+
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <u1 className="navbar-nav">
+          <li className="nav-it em- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/department">
+              Department
+            </NavLink>
+          </li> 
+                  
+        </u1>
+      </nav>
+
+      
+
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/department' element={<Department/>}/>
+       
+      </Routes>
+      
+     </div>
+    </BrowserRouter>
   );
 }
 
